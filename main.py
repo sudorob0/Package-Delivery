3a87aa1a5f64e6b3eea6ca6e37ba82c494a30af7
Robert Uhl <80855216+sudorob0@users.noreply.github.com>
2022-11-19 11:04:32 -0700

    init commit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import csv
+from pprint import pprint
 
+with open("./WGUPS Distance Table test.csv") as fp:
+    reader = csv.reader(fp, delimiter=",", quotechar='"')
+    # next(reader, None)  # skip the headers
+    data_read = [row for row in reader]
 
+pprint(data_read)
 
 
 
