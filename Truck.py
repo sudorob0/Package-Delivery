29f0711c9bd86ad48c1ce8e00c62cea6ff64d83a
Robert Uhl <80855216+sudorob0@users.noreply.github.com>
2022-12-01 23:25:05 -0700

    fully operational, comments added

--- Truck.py
+++ Truck.py
@@ -8,19 +8,21 @@ class Truck:
         load=None,
         mileage=0.0,
         address="4001 South 700 East",
+        delivered_packages = []
     ):
         """Creates a truck object"""
         self.capacity = capacity
         self.speed = speed
         self.load = load
         self.packages = packages
+        self.delivered_packages = delivered_packages
         self.mileage = mileage
         self.address = address
         self.depart_time = depart_time
         self.time = depart_time
 
     def __str__(self):
-        return "%s, %s, %s, %s, %s, %s, %s" % (
+        return "%s, %s, %s, %s, %s, %s, %s, %s" % (
             self.capacity,
             self.speed,
             self.load,
@@ -28,4 +30,5 @@ class Truck:
             self.mileage,
             self.address,
             self.depart_time,
+            self.delivered_packages
         )
--- main.py
+++ main.py
@@ -20,65 +20,84 @@ package_hash_table = HashTable.ChainingHashTable()
 # create truck instances, manually load packages and set departure times
 truck1 = Truck.Truck(
     packages=[1, 6, 13, 14, 15, 16, 20, 21, 22, 29, 30, 34, 37, 40],
-    depart_time = datetime.timedelta(hours=8, minutes=0)
+    depart_time=datetime.timedelta(hours=8, minutes=0),
 )
 
 truck2 = Truck.Truck(
     packages=[3, 12, 17, 18, 19, 23, 24, 26, 27, 35, 36, 38, 39],
-    depart_time = datetime.timedelta(hours=9, minutes=10)
-                     )
+    depart_time=datetime.timedelta(hours=9, minutes=10),
+)
 
 truck3 = Truck.Truck(
     packages=[2, 4, 5, 7, 8, 9, 10, 11, 25, 28, 31, 32, 33],
-    depart_time = datetime.timedelta(hours=11, minutes=5)
+    depart_time=datetime.timedelta(hours=11, minutes=5),
 )
 
 # Load packages objects into the hash table
 load_package_data(PACKAGE_FILE, package_hash_table)
 
 # variable to store total number of packages to be delivered
-num_of_packages = (len(truck1.packages) + len(truck2.packages) + len(truck3.packages))
+num_of_packages = len(truck1.packages) + len(truck2.packages) + len(truck3.packages)
+
 
 def deliver_packages(truck):
     """Method for ordering the packages in a truck using nearest neighbor algorithm"""
     # create empty list to hole packages not yet delivered
     not_delivered = []
-    for packageID in truck.packages:
-        package = package_hash_table.search(packageID)
+    # Loop through packages in a truck
+    for package_id in truck.packages:
+        # Find package in the hash table
+        package = package_hash_table.search(package_id)
+        # Add package to not_delivered list
         not_delivered.append(package)
+    # Clear package list
     truck.packages.clear()
-
+    # While loop to loop through all not_delivered packages
     while len(not_delivered) > 0:
         # set address to be longer than any address we will get, so it will be replaced
-        next_address = 10000
+        next_address_distance = 10000
         next_package = None
         for package in not_delivered:
-            if check_distance(get_address_id(truck.address), get_address_id(package.address)) <= next_address:
-                next_address = check_distance(get_address_id(truck.address), get_address_id(package.address))
+            """ 
+                Conditional statement that gets the current truck address and the package address 
+                and compares it to the distance of the next address
+                The first loop will hit because the next address is set really high
+            """
+            if (
+                check_distance(
+                    get_address_id(truck.address), get_address_id(package.address)
+                )
+                <= next_address_distance
+            ):
+                # If the current address is a shorter distance then it will replace next_address value
+                next_address_distance = check_distance(
+                    get_address_id(truck.address), get_address_id(package.address)
+                )
                 next_package = package
-        # Adds next closest package to the truck package list
+        # Adds the package with the smallest distance from current address to the truck
         truck.packages.append(next_package.id)
-        # Removes the same package from the not_delivered list
+        # Removes the delivered package from the not_delivered list
         not_delivered.remove(next_package)
-        # Takes the mileage driven to this packaged into the truck.mileage attribute
-        truck.mileage += next_address
+        # Updates the trucks total mileage
+        truck.mileage += next_address_distance
         # Updates truck's current address attribute to the package it drove to
         truck.address = next_package.address
-        # Updates the time it took for the truck to drive to the nearest address
-        truck.time += datetime.timedelta(hours=next_address / 18)
+        # Updates the trucks time with the time it took to deliver package
+        truck.time += datetime.timedelta(hours=next_address_distance / 18)
+        # Updates the package with the delivery and departure time of the truck
         next_package.delivery_time = truck.time
         next_package.departure_time = truck.depart_time
 
-
+# start delivering packages
 deliver_packages(truck1)
 deliver_packages(truck2)
-# The below line of code ensures that truck 3 does not leave until either of the first two trucks are finished
-# delivering their packages
+# truck3's departure time will start once both truck1&2 are done
 truck3.depart_time = min(truck1.time, truck2.time)
 deliver_packages(truck3)
 
+
 def main():
-    """"Function for the CLI"""
+    """ "Function for the CLI"""
     # start with a 7am time for current time
     current_time = datetime.timedelta(hours=7, minutes=0)
 
@@ -87,30 +106,31 @@ def main():
 
     # Main loop.  Prompts user for actions until exit is chosen.
     while True:
-        print(f''' 
+        print(
+            f""" 
         Current time is {current_time}
         1) Set time of day
         2) Current package status
         3) Truck status
         4) Single package status
-        0) Exit ''')
+        0) Exit """
+        )
         # user input
-        selection = input('Please enter the number of your selection:\n').strip()
+        selection = input("Please enter the number of your selection:\n").strip()
 
         # Conditional statement to allow user to give options
         if selection == "1":
-            hour = input(
-                "Please enter what hour it is (in military time):")
-            minute  = input(
-                "Please enter what minute it is:")
+            hour = input("Please enter what hour it is (in military time):")
+            minute = input("Please enter what minute it is:")
             # Update current time with user input
             current_time = datetime.timedelta(hours=int(hour), minutes=int(minute))
         elif selection == "2":
+            print(f"Current Time: {current_time}")
             # header
             print("ID\t| Deadline\t| Status\t\t| Departure\t\t| Delivery\t\t| Address")
-            print("-"*80)
+            print("-" * 80)
             # Loop through all packages
-            for package_id in range(1, num_of_packages):
+            for package_id in range(1, num_of_packages + 1):
                 # Find package in hash table
                 package = package_hash_table.search(package_id)
                 # Update package status
@@ -125,23 +145,36 @@ def main():
                 format = ""
                 if package.deadline == "EOD":
                     format = "\t"
-                print(f"{package.id}\t\t| {package.deadline}{format}\t| {package.status}\t| {timestamp}\t\t| {package.address}")
+                print(
+                    f"{package.id}\t| {package.deadline}{format}\t| {package.status}\t| {timestamp}\t\t| {package.address}"
+                )
         elif selection == "3":
-            print("Truck #\t\t| Departure\t| Mileage\t| Location\t\t| Packages")
+            print("Truck #\t\t| Departure\t| Mileage\t| Location\t\t\t| Packages")
             print("-" * 80)
-            print(f"Truck 1\t\t| {truck1.depart_time}\t| {truck1.mileage}\t\t| {truck1.address}\t| {truck1.packages}")
-            print(f"Truck 2\t\t| {truck2.depart_time}\t| {truck2.mileage}\t\t| {truck2.address}\t| {truck2.packages}")
-            print(f"Truck 3\t\t| {truck3.depart_time}\t| {truck3.mileage}\t\t| {truck3.address}\t| {truck3.packages}")
+            print(
+                f"Truck 1\t\t| {truck1.depart_time}\t| {truck1.mileage}\t\t| {truck1.address}\t| {truck1.packages}"
+            )
+            print(
+                f"Truck 2\t\t| {truck2.depart_time}\t| {truck2.mileage}\t\t| {truck2.address}\t\t| {truck2.packages}"
+            )
+            print(
+                f"Truck 3\t\t| {truck3.depart_time}\t| {truck3.mileage}\t\t| {truck3.address}\t\t| {truck3.packages}"
+            )
+            print(f"Total Mileage: {truck1.mileage + truck2.mileage + truck3.mileage}")
         elif selection == "4":
-            user_package_id = input('Enter package ID:\n').strip()
+            user_package_id = input("Enter package ID:\n").strip()
             package = package_hash_table.search(int(user_package_id))
             # Update departure and delivery time if packages are on its way or delivered
-            timestamp = "Departure Time: Awaiting pickup\nDelivery Time: Pending delivery"
+            timestamp = (
+                "Departure Time: Awaiting pickup\nDelivery Time: Pending delivery"
+            )
             if "Delivered" in package.status:
                 timestamp = f"Departure: {package.departure_time}\nDelivered: {package.delivery_time}"
             elif package.status == f"On it's way\t":
                 "Departure:{package.departure_time},\nDelivered:Pending delivery"
-            print(f"Current Time:{current_time}\nAddress: {package.address}\nCurrent Status: {package.status}\n{timestamp}")
+            print(
+                f"Current Time:{current_time}\nAddress: {package.address}\nCurrent Status: {package.status}\n{timestamp}"
+            )
         elif selection == "0":
             # Exit out of CLI
             "Your are now leaving the CLI. Goodbye."
@@ -149,5 +182,6 @@ def main():
         else:
             # Else for invalid entries
             print(f"{selection} is an invalid entry. Please try again.")
+
 
 main()
